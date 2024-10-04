1b1d8393ef1ea3e81e87a5f919dc149eda43199e
marsheltatiana <marsheltatianasanchezvigo@gmail.com>
2024-10-03 19:13:43 -0500

    refactor text size

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
       <section className="flex flex-col">
-      <h1 className="text-6xl font-bold">Adminitrador de archivos</h1>
+      <h1 className="text-xl lg:text-6xl font-bold">Adminitrador de archivos</h1>
         <h5 className="text-md font-[family-name:var(--font-geist-mono)]">Municipalidad Distrital de Guadalupe</h5>
       </section>
       
