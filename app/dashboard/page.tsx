ee8b7735b3ccaa6ac67451e67271430d282a3580
marsheltatiana <marsheltatianasanchezvigo@gmail.com>
2024-10-10 16:54:28 -0500

    feat: auth ui

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -13,9 +13,9 @@ export default async function DashboardPage() {
 
   return (
     <section className="flex flex-col">
-      <section className="pl-6 py-5">
+      <section className="pl-6 py-5 flex gap-4">
         <h1 className="text-lg">Hola <span className="font-semibold">{session?.user?.name}</span></h1>
-        <Image src={session?.user.image!} width={24} height={24} alt="avatar"/>
+        <Image src={session?.user.image!} width={40} height={40} alt="avatar" className="rounded-full"/>
       </section>
       <DashboardPageClient />
     </section>
