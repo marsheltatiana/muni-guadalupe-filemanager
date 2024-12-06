113bf793c941bdbaa431431b95be99f683b425e2
georgegiosue <peraldonamoc@gmail.com>
2024-12-06 01:59:17 -0500

    feat: implement user creation in NextAuth sign-in callback with default role assignment

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,52 @@
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
- 
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import prisma from "./db";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [Google],
-})
+  providers: [Google],
+  callbacks: {
+    signIn: async ({ user, account, profile }) => {
+      // check if the user exists in the database
+
+      const prismaUser = await prisma.usuario.findFirst({
+        where: {
+          email: user.email,
+        },
+      });
+
+      if (!prismaUser) {
+        // if the user does not exist, create it
+
+        const roleTrabajador = await prisma.rol.findFirst({
+          where: {
+            nombre_rol: "Trabajador",
+          },
+        });
+
+        if (!roleTrabajador) {
+          const role = await prisma.rol.create({
+            data: {
+              nombre_rol: "Trabajador",
+              descripcion: "Rol por defecto para los trabajadores.",
+            },
+          });
+        }
+
+        await prisma.usuario.create({
+          data: {
+            email: user.email,
+            nombre: user.name,
+            imagen_perfil: user.image,
+            Rol: {
+              connect: {
+                id_rol: roleTrabajador?.id_rol,
+              },
+            },
+          },
+        });
+      }
+
+      return true;
+    },
+  },
+});
