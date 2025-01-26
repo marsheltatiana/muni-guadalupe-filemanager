f6b6c4f9d9c2fb010dc29e7fcc353468ad273e9c
georgegiosue <peraldonamoc@gmail.com>
2025-01-26 15:01:18 -0500

    feat: enhance error handling in GET permisos route for improved reliability

--- app/api/permisos/route.ts
+++ app/api/permisos/route.ts
@@ -2,16 +2,23 @@ import prisma from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const permisos = await prisma.permisos.findMany();
+  try {
+    const permisos = await prisma.permisos.findMany();
 
-  if (permisos.length === 0) {
+    if (permisos.length === 0) {
+      return NextResponse.json(
+        { message: "No hay permisos registrados." },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(permisos);
+  } catch (error: any | unknown) {
     return NextResponse.json(
-      { message: "No hay permisos registrados." },
-      { status: 404 }
+      { message: `Error al optener los permisos ${error.message}` },
+      { status: 500 }
     );
   }
-
-  return NextResponse.json(permisos);
 }
 
 export async function POST(request: NextRequest) {
