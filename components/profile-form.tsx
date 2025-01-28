b3eeeda9e41ab5e1f44cb0b027708aa28e85a76b
georgegiosue <peraldonamoc@gmail.com>
2025-01-28 09:18:22 -0500

    feat: simplify profile form by removing registration toggle and enhancing sign-in flow

--- components/profile-form.tsx
+++ components/profile-form.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -10,51 +9,26 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import Link from "next/link";
-import React from "react";
 import { SignInForm } from "./signin-form";
-import { SignUpForm } from "./signup-form";
 
 export function ProfileForm() {
-  const [onRegister, setOnRegister] = React.useState(false);
-
   return (
     <Card className="w-full md:max-w-[450px]">
       <CardHeader>
         <CardTitle>Municipalidad Distrital de Guadalupe</CardTitle>
         <CardDescription>
-          {onRegister
-            ? "Registrate para acceder al archivo general."
-            : "Inicia sesión para acceder al archivo general."}
+          Inicia sesión para acceder al archivo general
         </CardDescription>
       </CardHeader>
       <CardContent className="space-y-3">
-        {onRegister ? <SignUpForm /> : <SignInForm />}
-        {onRegister ? (
-          <Button
-            onClick={() => setOnRegister(false)}
-            variant="secondary"
-            className="w-full"
-          >
-            Iniciar sesión
-          </Button>
-        ) : (
-          <Button
-            onClick={() => setOnRegister(true)}
-            variant="secondary"
-            className="w-full"
-          >
-            Registrarse
-          </Button>
-        )}
+        <SignInForm />
       </CardContent>
       <CardFooter className="flex gap-3">
-        {!onRegister && (
-          <Link href={"#"}>
-            <span className="text-sm text-primary underline-offset-4 underline">
-              ¿Olvidaste tu contraseña?
-            </span>
-          </Link>
-        )}
+        <Link href={"#"}>
+          <span className="text-sm text-primary underline-offset-4 underline">
+            ¿Olvidaste tu contraseña?
+          </span>
+        </Link>
         <Link
           href="https://muni-guadalupe-filemanager-documentacion.vercel.app/"
           target="_blank"
